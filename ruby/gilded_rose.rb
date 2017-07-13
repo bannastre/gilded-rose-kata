6255848198c90ec3790e4cc61f01df6731f495fc
Christopher Harrop <bannastre@gmail.com>
2017-07-13 14:37:23 +0100

    Class refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,29 +6,16 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-
-      case item.name
-      when "Sulfuras, Hand of Ragnaros"
-      when "Aged Brie"
-        item.sell_in > 0 ? item.quality += 1 : item.quality += 2
-        item.sell_in -= 1
-      when "Backstage passes to a TAFKAL80ETC concert"
-        item.sell_in < 11 ? item.quality += 1 : item.quality
-        item.sell_in < 6 ? item.quality += 1 : item.quality
-        item.sell_in > 0 ? item.quality += 1 : item.quality = 0
-        item.sell_in -= 1
-      else
-        item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
-        item.sell_in -= 1
-      end
+      item.update_quality
 
       item.quality > 50 ? item.quality = 50 : item.quality = item.quality
       item.quality < 0 ? item.quality = 0 : item.quality = item.quality
     end
   end
-end #class
 
-class Item
+end
+
+class NormalItem
   attr_accessor :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
@@ -37,7 +24,68 @@ class Item
     @quality = quality
   end
 
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+  def update_quality
+    @sell_in > 0 ? @quality -= 1 : @quality -= 2
+    @sell_in -= 1
+  end
+end
+
+class Sulfuras
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def update_quality
+  end
+end
+
+class AgedBrie
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def update_quality
+    @sell_in > 0 ? @quality += 1 : @quality += 2
+    @sell_in -= 1
+  end
+end
+
+class BackStagePass
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def update_quality
+    @sell_in < 11 ? @quality += 1 : @quality
+    @sell_in < 6 ? @quality += 1 : @quality
+    @sell_in > 0 ? @quality += 1 : @quality = 0
+    @sell_in -= 1
+  end
+end
+
+class ConjouredItem
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def update_quality
+    @sell_in > 0 ? @quality -= 2 : @quality -= 4
+    @sell_in -= 1
   end
 end
